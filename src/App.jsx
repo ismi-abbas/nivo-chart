3210d262ef4c44873873cc8f78bd536fad14d182
Abbas <muhdabbas98@gmail.com>
2023-12-17 17:36:02 +0800

    Graph

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { ResponsiveCalendar } from '@nivo/calendar';
+import data from './data.json';
+import { useEffect, useState } from 'react';
+import './App.css';
+
+function App() {
+	const [codeStats, setCodeStats] = useState(data);
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const fetchData = async () => {
+		const response = await fetch('https://codestats.net/api/users/ismiabbas');
+		const responseData = await response.json();
+		const dates = responseData.dates;
+
+		let dateData = [];
+
+		for (const key in dates) {
+			if (Object.hasOwnProperty.call(dates, key)) {
+				dateData.push({
+					day: key,
+					value: dates[key],
+				});
+			}
+		}
+		setCodeStats(dateData);
+	};
+
+	return (
+		<div className="wrapper">
+			<h1>My Codestats exp</h1>
+			<a href="https://codestats.net/users/ismiabbas">Link</a>
+
+			<div className="container">
+				<ResponsiveCalendar
+					data={codeStats}
+					from="2023-01-01"
+					to="2023-12-31"
+					emptyColor="#eeeeee"
+					colors={['#216e39', '#30a14e', '#40c463', '#9be9a8'].reverse()}
+					margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
+					yearSpacing={40}
+					monthBorderColor="#ffffff"
+					dayBorderWidth={2}
+					dayBorderColor="#ffffff"
+					legends={[
+						{
+							anchor: 'top',
+							direction: 'row',
+							translateY: 36,
+							itemCount: 4,
+							itemWidth: 42,
+							itemHeight: 12,
+							itemsSpacing: 24,
+						},
+					]}
+				/>
+			</div>
+		</div>
+	);
+}
+
+export default App;
